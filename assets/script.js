dbd4df932d3347988271ff82b12ef37c328093d4
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-02-29 21:04:49 -0500

    Updated readme

--- assets/script.js
+++ assets/script.js
@@ -4,37 +4,37 @@ $(document).ready(function () {
     //Display current day using current day id
     $("#currentDay").text(today.format("dddd, MMMM Do"));
     //variable for current time from moment to change color of past present future
-    var now = parseInt(moment().format('H'));
+    let now = parseInt(moment().format('H'));
     console.log(now)
 
 
     //Variable for all inputs in each time box for local storage
-    var $userInput9 = $("#userInput9");
-    var $userInput10 = $("#userInput10");
-    var $userInput11 = $("#userInput11");
-    var $userInput12 = $("#userInput12");
-    var $userInput1 = $("#userInput1");
-    var $userInput2 = $("#userInput2");
-    var $userInput3 = $("#userInput3");
-    var $userInput4 = $("#userInput4");
-    var $userInput5 = $("#userInput5");
+    let $userInput9 = $("#userInput9");
+    let $userInput10 = $("#userInput10");
+    let $userInput11 = $("#userInput11");
+    let $userInput12 = $("#userInput12");
+    let $userInput1 = $("#userInput1");
+    let $userInput2 = $("#userInput2");
+    let $userInput3 = $("#userInput3");
+    let $userInput4 = $("#userInput4");
+    let $userInput5 = $("#userInput5");
 
-
+    //color change function
     $("textarea").each(function () {
         for (let hour = 9; hour <= 17;hour++){
             if (hour < now) {
-                $(this).addClass("bg-gray");
+                $(this).css(".past");
             }
             if (hour > now) {
-                $(this).addClass("bg-green")
+                $(this).css(".present");
             }
             if (hour === now) {
-                $(this).addClass("bg-red")
+                $(this).css(".future");
             }
         }
         
     })
-
+    //on click for save button to store info in local storage 
     $("button").on("click", function () {
 
         //setting items in the local storage
@@ -50,7 +50,7 @@ $(document).ready(function () {
 
     })
 
-    //getting the content stored and sending to the screen. When page is refreshed content will stay
+    //Saving user input in local storage even after refresh
     $("#userInput9").append(localStorage.getItem("9AM"));
     $("#userInput10").append(localStorage.getItem("10AM"));
     $("#userInput11").append(localStorage.getItem("11AM"));
